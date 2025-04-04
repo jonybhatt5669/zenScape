9fc6e2d6dae41c9badce608e0404f4459728c261
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-04-04 11:55:24 +0600

    Search functionality

--- app/(music)/index.tsx
+++ app/(music)/index.tsx
@@ -1,15 +1,29 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { Link, Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text, Image, Pressable, FlatList } from 'react-native';
+import { useMemo } from 'react';
+import { FlatList, Image, Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import library from '../../assets/data/library.json';
 
 import { unknownAlbumImageUri } from '~/src/constants/images';
+import { useNavigationSearch } from '~/src/hooks/useNavigationSearch';
 import { IMusicTrack } from '~/src/interfaces/MusicTrackInterface';
+import { trackTitleFilter } from '~/src/utils/filterTracks';
 
 export default function MusicScreen() {
   const { mood } = useLocalSearchParams();
+  const search = useNavigationSearch({
+    options: {
+      placeholder: 'Search for tracks',
+      hideWhenScrolling: false,
+      textColor: '#fff',
+    },
+  });
+  const filterList = useMemo(() => {
+    if (!search) return library;
+    return library.filter(trackTitleFilter(search));
+  }, [search]);
   const renderTrack = ({ item }: { item: IMusicTrack }) => (
     <View className="flex-row items-center rounded bg-secondary-900 px-4 py-2">
       {/* Album Art */}
@@ -19,7 +33,9 @@ export default function MusicScreen() {
       />
       {/* Track Info */}
       <View className="flex-1">
-        <Text className="text-base font-semibold text-white">{item.title}</Text>
+        <Text numberOfLines={1} className="text-base font-semibold text-white">
+          {item.title}
+        </Text>
         <Text className="text-sm text-gray-400">{item.artist}</Text>
       </View>
       {/* Play Button */}
@@ -41,33 +57,41 @@ export default function MusicScreen() {
   );
 
   return (
-    <SafeAreaView className="flex-1 bg-primary-400">
+    <>
       <Stack.Screen
         options={{
-          headerStyle: {
+          headerLargeStyle: {
             backgroundColor: '#1f1f20',
           },
+          headerLargeTitle: true,
           headerTintColor: '#fff',
-          title: 'Playlists',
+          title: ` ${mood} Playlists `,
+          headerTransparent: true,
+          headerBlurEffect: 'dark',
+          headerShadowVisible: false,
         }}
       />
-      <View className="border-b border-gray-700 p-4">
+      <SafeAreaView className="flex-1 bg-primary-400 ">
+        {/* <View className="border-b border-gray-700 p-4">
         <Text className="text-3xl font-semibold text-neutral-100">Music for {mood}</Text>
-      </View>
-      <FlatList
-        data={library}
-        style={{
-          gap: 4,
-          marginHorizontal: 10,
-        }}
-        renderItem={renderTrack}
-        keyExtractor={(item) => item.url}
-        contentContainerStyle={{
-          paddingVertical: 10,
-          gap: 6,
-          borderRadius: 10,
-        }}
-      />
-    </SafeAreaView>
+      </View> */}
+
+        <FlatList
+          data={filterList}
+          style={{
+            gap: 4,
+            marginHorizontal: 10,
+          }}
+          renderItem={renderTrack}
+          keyExtractor={(item) => item.url}
+          contentContainerStyle={{
+            paddingTop: 50,
+            paddingBottom: 100,
+            gap: 6,
+            borderRadius: 10,
+          }}
+        />
+      </SafeAreaView>
+    </>
   );
 }

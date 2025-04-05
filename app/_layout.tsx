f4675b461d2a2e9eaaf911f45bf8f7293f82c67d
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-04-05 08:11:16 +0600

    Fix layout issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,22 @@ import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(music)"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
